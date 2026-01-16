9a133b8e8c2c3f38c4c2ac6af5fc643a79ff2b06
TheSunKng <asemotadavid486@gmail.com>
2026-01-16 16:46:13 +0100

    Added whitenoise to render page on prod

--- sems_auth_service/settings.py
+++ sems_auth_service/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -132,6 +133,10 @@ SWAGGER_SETTINGS = {
         }
     }
 }
+
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 # Internationalization
